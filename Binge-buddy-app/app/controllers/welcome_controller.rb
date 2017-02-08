dbec0be8b737f60ca49df3c18030e8dfb00869ec
Kenza Mandri <kenzamandri@gmail.com>
2017-02-08 12:03:16 -0500

    api key committed

--- Binge-buddy-app/app/controllers/welcome_controller.rb
+++ Binge-buddy-app/app/controllers/welcome_controller.rb
@@ -3,7 +3,7 @@ class WelcomeController < ApplicationController
   def home
     @message = "yo welcome to binging and budging!"
     @guidebox = HTTParty.get("http://api-public.guidebox.com/v2/movies?api_key=#{ENV['GUIDEBOXWRAPPER_API_KEY']}&limit=15")
-    puts "CHECK", @guidebox.inspect
+    puts "CHECK", "http://api-public.guidebox.com/v2/movies?api_key=#{ENV['GUIDEBOXWRAPPER_API_KEY']}&limit=15"
 
 
 
